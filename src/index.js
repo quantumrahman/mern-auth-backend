6ddb44e86c490e8cf3f7ee3c4d0eeee0f561bc2e
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2025-12-22 13:52:33 +0600

    import database connection function in index.js file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 // import modules ----------------------------------------->
 import chalk from 'chalk';
 import app from './app.js';
+import dbConnect from './database/db.connect.js';
 
 // function ----------------------------------------------->
 const startServer = () => {
     try {
+        dbConnect();
+
         const port = process.env.PORT || 4000;
         app.listen(port, () => {
             console.log(chalk.green(`Server running on http://localhost:${port}`));
