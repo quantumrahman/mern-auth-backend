e89e4220052e0379eef0dd35b2b2fe3e22b449a7
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2025-12-24 21:28:05 +0600

    modify function parameters

--- src/utils/generator/jwt.generator.js
+++ src/utils/generator/jwt.generator.js
@@ -2,7 +2,7 @@
 import jwt from 'jsonwebtoken';
 
 // generator ---------------------------------------------->
-const generatorJwt = (userId, userEmail) => {
+const generatorJwt = ({ userId, userEmail }) => {
     return jwt.sign({ userId, userEmail }, process.env.JWT_SECRET, {
         expiresIn: '7d'
     });
