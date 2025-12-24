95acc1624e9f0409208762d80bd5125a666dbad6
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2025-12-24 11:44:11 +0600

    create auth.routes.js file and implement express routing function

---
+++ src/routes/auth.routes.js
@@ -0,0 +1,11 @@
+// import modules ----------------------------------------->
+import express from 'express';
+
+// router ------------------------------------------------->
+const router = express.Router();
+
+// routes ------------------------------------------------->
+router.route('/sign_up').post('');
+
+// export modules ----------------------------------------->
+export default router;
