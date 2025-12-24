4392c212cdf867182bce21359645ed3d015ad9b4
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2025-12-24 12:06:40 +0600

    create auth.controller.js file and implement sign up controller function

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,7 @@
+// import modules ----------------------------------------->
+import 'dotenv/config';
+
+// signup controller -------------------------------------->
+export const signUpController = async (req, res, next) => {
+    console.log('Hello from sign up controller.');
+};
