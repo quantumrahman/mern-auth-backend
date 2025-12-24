c496e00c42d57e0dad96f6ecc48be255806d0b35
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2025-12-24 12:38:44 +0600

    create email.validator.js file and implement email validation function

---
+++ src/utils/validator/email.validator.js
@@ -0,0 +1,9 @@
+// validator ---------------------------------------------->
+const emailValidator = (email) => {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const isValid = emailRegex.test(email);
+    return isValid;
+};
+
+// export modules ----------------------------------------->
+export default emailValidator;
