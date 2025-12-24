53998b50a48cbb4068b102415e5ddfc6f7d400d6
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2025-12-24 13:29:35 +0600

    create jwt.generator.js file and implement jwt token generator function

---
+++ src/utils/generator/jwt.generator.js
@@ -0,0 +1,12 @@
+// import modules ----------------------------------------->
+import jwt from 'jsonwebtoken';
+
+// generator ---------------------------------------------->
+const generatorJwt = (userId, userEmail) => {
+    return jwt.sign({ userId, userEmail }, process.env.JWT_SECRET, {
+        expiresIn: '7d'
+    });
+};
+
+// export modules ----------------------------------------->
+export default generatorJwt;
