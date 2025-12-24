dfe44247cbc5323657fcced753280fe4f5042563
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2025-12-24 12:07:38 +0600

    import sign up controller in auth.routes.js file

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,12 @@
 // import modules ----------------------------------------->
 import express from 'express';
+import { signUpController } from '../controllers/auth.controller';
 
 // router ------------------------------------------------->
 const router = express.Router();
 
 // routes ------------------------------------------------->
-router.route('/sign_up').post('');
+router.route('/sign_up').post(signUpController);
 
 // export modules ----------------------------------------->
 export default router;
