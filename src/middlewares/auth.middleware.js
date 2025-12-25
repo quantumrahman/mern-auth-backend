030911194cd8c186d997878cca5c20d433d4cb79
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2025-12-25 13:51:32 +0600

    auth middleware completed

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,10 +1,41 @@
 // import modules ----------------------------------------->
-import AppError from '../utils/constructors/error.constructor.js';
+import jwt from 'jsonwebtoken';
 import User from '../models/user.model.js';
+import AppError from '../utils/constructors/error.constructor.js';
 
 // middleware --------------------------------------------->
 const authMiddleware = async (req, res, next) => {
+    try {
+        const { token } = req.cookies;
 
+        if (!token) throw new AppError("Authentication token missing!", {
+            status: 401,
+            code: "UNAUTHORIZED"
+        });
+
+        let decoded;
+
+        try {
+            decoded = jwt.verify(token, process.env.JWT_SECRET);
+        } catch (error) {
+            throw new AppError("Invalid or expired token!", {
+                status: 401,
+                code: "UNAUTHORIZED"
+            });
+        }
+
+        const user = await User.findById(decoded.id);
+
+        if (!user) throw new AppError("User not found!", {
+            status: 401,
+            code: "UNAUTHORIZED"
+        });
+
+        req.user = user;
+        next();
+    } catch (error) {
+        next(error);
+    }
 };
 
 // export modules ----------------------------------------->
