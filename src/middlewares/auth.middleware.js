aa15d83ecedc40d53d801de3c01247ec148630fd
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2025-12-25 13:34:58 +0600

    create auth.middleware.js file

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,11 @@
+// import modules ----------------------------------------->
+import AppError from '../utils/constructors/error.constructor.js';
+import User from '../models/user.model.js';
+
+// middleware --------------------------------------------->
+const authMiddleware = async (req, res, next) => {
+
+};
+
+// export modules ----------------------------------------->
+export default authMiddleware;
