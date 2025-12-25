11d5ccf55f541c5f8bd6c578dbfff7f171003313
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2025-12-25 11:32:22 +0600

    import signOutController in auth.routes.js file

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,6 +1,6 @@
 // import modules ----------------------------------------->
 import express from 'express';
-import { signUpController, signInController } from '../controllers/auth.controller.js';
+import { signUpController, signInController, signOutController } from '../controllers/auth.controller.js';
 
 // router ------------------------------------------------->
 const router = express.Router();
@@ -8,6 +8,7 @@ const router = express.Router();
 // routes ------------------------------------------------->
 router.route('/sign_up').post(signUpController);
 router.route('/sign_in').post(signInController);
+router.route('/sign_out').get(signOutController);
 
 // export modules ----------------------------------------->
 export default router;
